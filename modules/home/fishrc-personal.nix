ae57bd2ba536f0b85d3d9578310fd69f36b2bd2e
DavidV <dvogelca@proton.me>
2025-06-03 06:50:15 -0400

    fix(fish): add ssh auth sock
    Signed-off-by: DavidV <dvogelca@proton.me>

--- modules/home/fishrc-personal.nix
+++ modules/home/fishrc-personal.nix
@@ -6,6 +6,7 @@
       #!/usr/bin/env fish
     set -gx GDK_SCALE "1"
     set -gx QT_SCALE_FACTOR "1"
-
+    set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    gpgconf --launch gpg-agent
   '';
 }
