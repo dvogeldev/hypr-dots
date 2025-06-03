1d32f262d2c2aa177612d686070f589db616fd50
DavidV <dvogelca@proton.me>
2025-06-03 06:41:00 -0400

    feat(fish): add vars and aliases
    Signed-off-by: DavidV <dvogelca@proton.me>

--- modules/home/fishrc-personal.nix
+++ modules/home/fishrc-personal.nix
@@ -1,18 +1,11 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [ fish ];
+  home.packages = with pkgs; [fish];
 
   home.file."./.fishrc-personal".text = ''
 
-  # This file allows you to define your own aliases, functions, etc
-  # below are just some examples of what you can use this file for
-
-    #!/usr/bin/env fish
-    # Set defaults
-    #
-    #set -gx EDITOR "nvim"
-    #set -gx VISUAL "nvim"
-
-    #alias c "clear"
+      #!/usr/bin/env fish
+    set -gx GDK_SCALE "1"
+    set -gx QT_SCALE_FACTOR "1"
 
   '';
 }
