a8f5c7468c41e5519507f92547037838c1020cfe
DavidV <dvogelca@proton.me>
2025-06-02 19:27:55 -0500

    init

---
+++ hosts/dvpc/variables.nix
@@ -0,0 +1,54 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "DavidV";
+  gitEmail = "dvogelca@proton.me";
+
+  # Hyprland Settings
+  # ex: extraMonitorSettings = "monitor = Virtual-1,1920x1080@60,auto,1";
+  # ex: extraMonitorSettings = "monitor = HDMI-A-1,1920x1080@60,auto,1";
+  extraMonitorSettings = "
+    monitor = DP-1,3840x2160,0x0,1.6
+    monitor = DP-2,3840x2160,3840x0,1.6
+  ";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+  consoleKeyMap = "us";
+
+  # For Nvidia Prime support
+  intelID = "PCI:1:0:0";
+  nvidiaID = "PCI:0:2:0";
+
+  # Enable NFS
+  enableNFS = false;
+
+  # Enable Printing Support
+  printEnable = false;
+
+  # Set Stylix Image
+  stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
+
+  # Set Waybar
+  #  Available Options:
+  waybarChoice = ../../modules/home/waybar/waybar-curved.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-ddubs.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-ddubs-2.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-simple.nix;
+  #waybarChoice = ../../modules/home/waybar/Jerry-waybar.nix;
+
+  # Set Animation style
+  # Available options are:
+  # animations-def.nix  (default)
+  # animations-end4.nix (end-4 project)
+  # animations-dynamic.nix (ml4w project)
+  # animations-moving.nix (ml4w project)
+  animChoice = ../../modules/home/hyprland/animations-def.nix;
+
+  # Enable Thunar GUI File Manager
+  thunarEnable = true;
+}
