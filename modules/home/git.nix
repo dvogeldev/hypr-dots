6f47368ea497a30c421940117092455043489108
DavidV <dvogelca@proton.me>
2025-06-06 08:00:52 -0400

    feat(git): add extra config
    Signed-off-by: DavidV <dvogelca@proton.me>

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,11 +1,17 @@
-{ host, ... }:
-let
+{host, ...}: let
   inherit (import ../../hosts/${host}/variables.nix) gitUsername gitEmail;
-in
-{
+in {
   programs.git = {
     enable = true;
     userName = "${gitUsername}";
     userEmail = "${gitEmail}";
+    extraConfig = {
+      push.default = "simple";
+      credential.helper = "cache --timeout=7200";
+      init.defaultBranch = "main";
+      log.decorate = "full";
+      log.date = "iso";
+      merge.conflictStyle = "diff3";
+    };
   };
 }
