c6d7e610ddd2bfad592053bce4676d6c696170a4
DavidV <dvogelca@proton.me>
2025-06-09 10:14:33 -0400

    feat(emacs): add flakes for emacs and doom emacs
    Signed-off-by: DavidV <dvogelca@proton.me>

--- flake.nix
+++ flake.nix
@@ -8,66 +8,76 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nvf.url = "github:notashelf/nvf";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
     stylix.url = "github:danth/stylix/release-25.05";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: let
-    system = "x86_64-linux";
-    host = "dvpc";
-    profile = "nvidia";
-    username = "david";
-  in {
-    nixosConfigurations = {
-      amd = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit inputs;
-          inherit username;
-          inherit host;
-          inherit profile;
+  outputs =
+    {
+      nixpkgs,
+      emacs-overlay,
+      nix-doom-emacs,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      host = "dvpc";
+      profile = "nvidia";
+      username = "david";
+    in
+    {
+      nixosConfigurations = {
+        amd = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            inherit username;
+            inherit host;
+            inherit profile;
+          };
+          modules = [ ./profiles/amd ];
         };
-        modules = [./profiles/amd];
-      };
-      nvidia = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit inputs;
-          inherit username;
-          inherit host;
-          inherit profile;
+        nvidia = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            inherit username;
+            inherit host;
+            inherit profile;
+          };
+          modules = [ ./profiles/nvidia ];
         };
-        modules = [./profiles/nvidia];
-      };
-      nvidia-laptop = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit inputs;
-          inherit username;
-          inherit host;
-          inherit profile;
+        nvidia-laptop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            inherit username;
+            inherit host;
+            inherit profile;
+          };
+          modules = [ ./profiles/nvidia-laptop ];
         };
-        modules = [./profiles/nvidia-laptop];
-      };
-      intel = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit inputs;
-          inherit username;
-          inherit host;
-          inherit profile;
+        intel = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            inherit username;
+            inherit host;
+            inherit profile;
+          };
+          modules = [ ./profiles/intel ];
         };
-        modules = [./profiles/intel];
-      };
-      vm = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit inputs;
-          inherit username;
-          inherit host;
-          inherit profile;
+        vm = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            inherit username;
+            inherit host;
+            inherit profile;
+          };
+          modules = [ ./profiles/vm ];
         };
-        modules = [./profiles/vm];
       };
     };
-  };
 }
