39bea10047a9503cd436fb89916b2286179c803b
DavidV <dvogelca@proton.me>
2025-06-18 14:05:16 -0400

    feat(ssh): added aliases to local machines
    Signed-off-by: DavidV <dvogelca@proton.me>

--- modules/core/ssh.nix
+++ modules/core/ssh.nix
@@ -4,11 +4,13 @@
     Host dvpc
       Hostname 192.168.1.210
       User david
+      RequestTTY force
       # If the port is not 22, uncomment and set it like this:
       # Port 19999
     Host dvtp
       Hostname 192.168.1.205
       User david
+      RequestTTY force
       # If the port is not 22, uncomment and set it like this:
       # Port 19999
   '';
