2e75f3745eeccbd6e97e59549140be5eb7e056e3
DavidV <dvogelca@proton.me>
2025-06-18 14:34:27 -0400

    feat(fuzzel): settings conflicting with stylix
    Signed-off-by: DavidV <dvogelca@proton.me>

--- modules/home/fuzzel.nix
+++ modules/home/fuzzel.nix
@@ -8,16 +8,16 @@ let
   inherit (import ../../../hosts/${host}/variables.nix) terminal;
 in
 {
-  home.packages = with pkgs; [
-    fuzzel
-  ];
+  # home.packages = with pkgs; [
+  #   fuzzel
+  # ];
 
   programs.fuzzel = {
     enable = true;
     settings = {
       main = {
         # Appearance
-        font = "monospace:size=12";
+        # font = "monospace:size=12";
         dpi-aware = "yes";
         prompt = "> ";
         icon-theme = "Papirus";
@@ -36,16 +36,16 @@ in
         no-exit-on-keyboard-focus-loss = "no";
       };
 
-      colors = {
-        # Using stylix colors for consistency
-        background = "${config.lib.stylix.colors.base00}dd";
-        text = "${config.lib.stylix.colors.base05}ff";
-        match = "${config.lib.stylix.colors.base08}ff";
-        selection = "${config.lib.stylix.colors.base02}ff";
-        selection-text = "${config.lib.stylix.colors.base05}ff";
-        selection-match = "${config.lib.stylix.colors.base08}ff";
-        border = "${config.lib.stylix.colors.base08}ff";
-      };
+      # colors = {
+      #   # Using stylix colors for consistency
+      #   # background = "${config.lib.stylix.colors.base00}dd";
+      #   text = "${config.lib.stylix.colors.base05}ff";
+      #   match = "${config.lib.stylix.colors.base08}ff";
+      #   selection = "${config.lib.stylix.colors.base02}ff";
+      #   selection-text = "${config.lib.stylix.colors.base05}ff";
+      #   selection-match = "${config.lib.stylix.colors.base08}ff";
+      #   border = "${config.lib.stylix.colors.base08}ff";
+      # };
 
       border = {
         width = 2;
